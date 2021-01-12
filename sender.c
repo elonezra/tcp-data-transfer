5470081b34652ba3679f47bb5954a7ba8804fda0
elonezra <elonezra1995@hotmail.com>
2021-01-12 19:37:48 +0200

    Update sender.c

--- sender.c
+++ sender.c
@@ -124,6 +124,7 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_port = htons(DEFAULT_PORT);
 
     // Connect and set nonblocking and nodelay
+    // initiate a connection on a socket
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) {
         error("ERROR connecting");
     }
